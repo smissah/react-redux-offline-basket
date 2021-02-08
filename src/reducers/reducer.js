63420dfff217164931ffc2d27dd5d4cfba2c637e
stephenPC <stephenmissah@outlook.com>
2021-02-08 15:09:45 +0000

    ternary used for decrease button

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -31,29 +31,22 @@ export const reducer = (state, action) => {
   }
 
   if (action.type === DECREASE) {
-    let tempCart = [];
-    if (action.payload.amount === 1) {
-      tempCart = state.cart.filter((cartItem) => {
-        return cartItem.id !== action.payload.id;
-      });
-    } else {
-      tempCart = state.cart.map((cartItem) => {
-        if (cartItem.id === action.payload.id) {
-          cartItem = {
-            ...cartItem,
-            amount: cartItem.amount - 1,
-          };
-        }
-        return cartItem;
-      });
-      console.log(`new temp cart is ${tempCart}`);
-    }
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload.id) {
+        cartItem = {
+          ...cartItem,
+          amount: cartItem.amount - 1,
+        };
+      }
+      return cartItem;
+    });
 
     return {
       ...state,
       cart: tempCart,
     };
   }
+
   if (action.type === REMOVE_ITEM) {
     const itemId = action.payload.id;
 
